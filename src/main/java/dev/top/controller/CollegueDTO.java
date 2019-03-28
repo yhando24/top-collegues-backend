36dfbfd3b91e5cf490b0c7145f223fd7998a6641
yhando24 <youcef.handoura@gmail.com>
2019-03-28 17:26:42 +0100

    besoin faire recherche url

---
+++ src/main/java/dev/top/controller/CollegueDTO.java
@@ -0,0 +1,46 @@
+package dev.top.controller;
+
+public class CollegueDTO {
+		
+	
+	 private Integer matricule;
+	 private String pseudo;
+
+	 private String photoUrl;
+
+	public Integer getMatricule() {
+		return matricule;
+	}
+
+	public void setMatricule(Integer matricule) {
+		this.matricule = matricule;
+	}
+
+	public String getPseudo() {
+		return pseudo;
+	}
+
+	public void setPseudo(String pseudo) {
+		this.pseudo = pseudo;
+	}
+
+	public String getPhotoUrl() {
+		return photoUrl;
+	}
+
+	public void setPhotoUrl(String photoUrl) {
+		this.photoUrl = photoUrl;
+	}
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+}
+
+
+
--- src/main/java/dev/top/controller/ColleguesController.java
+++ src/main/java/dev/top/controller/ColleguesController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -38,6 +39,13 @@ public class ColleguesController {
     public Collegue voter(@PathVariable String pseudo, @RequestBody VoteWeb action){
           return  collegueService.setActionToCollegue(pseudo, action.getAction());
     }
+    
+    @PostMapping
+    public CollegueDTO AjoutCollegue(@RequestBody CollegueDTO c) {
+		return c;
+    	
+    }
+
 
 
 }
